82695903af5c73e43ef2c5b1f6a391acb878d502
Denis <denis.beauget@etu.umontpellier.fr>
2020-11-09 12:26:16 +0100

    Start getRessources for tier4

--- PNJCalculator.py
+++ PNJCalculator.py
@@ -7,7 +7,7 @@ import PySimpleGUI as ms
 import requests
 
 
-print(need.getT6JournalMage())
+
 
 
 
@@ -16,11 +16,26 @@ print(need.getT6JournalMage())
 
 def rentability(values):
 
+    city = values['City']
+    BlackSmith = values['BlacksmithNumber']
+    #BlackSmith treatment
+    result = {}
 
+    if values['BlacksmithTier'] == 'Tier4':
+        result = need.getT4Data(city)
+    elif values['BlacksmithTier'] == 'Tier5':
+         result = need.getT5Data(city)
+    elif values['BlacksmithTier'] == 'Tier6':
+         result = need.getT6Data(city)
+    elif values['BlacksmithTier'] == 'Tier7':
+         result = need.getT7Data(city)
+    elif values['BlacksmithTier'] == 'Tier8':
+         result = need.getT8Data(city)
+    else:
+        print("aled")
     headings = ['PNJ','Tier','Rentability','1/3']
     header = [[ms.Text(' ')] + [ms.Text(h,size=(14,1)) for h in headings]]
-    res = values[0] * 2
-    input_rows = [[ms.Text(res,size=(15,1), pad=(0,0)) for col in range(4)] for row in range(4)]
+    input_rows = [[ms.Text(size=(15,1), pad=(0,0)) for col in range(4)] for row in range(4)]
 
     layout1 = header + input_rows
     window = ms.Window('PNJRentability', layout1,margins=(200,200))
@@ -32,11 +47,11 @@ ms.theme("Dark")
 #Stuff window
 layout = [
     [ms.Text('Welcome to PNJCalculator')],
-    [ms.Text('BlackSmith'),ms.InputText(size=(5,10)),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'])],
-    [ms.Text('Imbuer'),ms.InputText(size=(5,10)),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'])],
-    [ms.Text('Tinker'),ms.InputText(size=(5,10)),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'])],
-    [ms.Text('Fletcher'),ms.InputText(size=(5,10)),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'])],
-    [ms.Button('See rentability')]
+    [ms.Text('BlackSmith'),ms.InputText(size=(5,10),key='BlacksmithNumber'),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'],key='BlacksmithTier')],
+    [ms.Text('Imbuer'),ms.InputText(size=(5,10),key='ImbuerNumber'),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'],key='ImbuerTier')],
+    [ms.Text('Tinker'),ms.InputText(size=(5,10),key='TinkerNumber'),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'],key='TinkerTier')],
+    [ms.Text('Fletcher'),ms.InputText(size=(5,10),key='FletcherNumber'),ms.Text('Tier'),ms.Combo(['Tier4','Tier5','Tier6','Tier7','Tier8'],key='FletcherTier')],
+    [ms.Button('See rentability'),ms.Text('City'),ms.Combo(['Fortsterling','Martlock','Lymhurst','Bridgewatch','Thetford','Caerleon'],key='City')]
 ]
 
 window = ms.Window('PNJCalculator', layout,margins=(200,200))
--- data.py
+++ data.py
@@ -3,7 +3,53 @@
 import requests
 import json
 
-def getT6JournalMage(): 
+def getT4Data(city): 
+    #BooksData
+    result = {}
+    data = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_JOURNAL_MAGE_EMPTY?locations="+city+"&qualities=0")
+    data2 = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_JOURNAL_MAGE_FULL?locations="+city+"&qualities=0")
+    emptyBookPrice = data.json()
+    fullBookPrice = data2.json()
+    result["emptyBookPrice"] = emptyBookPrice[0]['sell_price_min']
+    result["fullBookPrice"] = fullBookPrice[0]['sell_price_min']
+
+    #RessourcesData
+    T4bar = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_METALBAR?locations="+city+"&qualities=0")
+    T4barPrice = T4bar.json()
+    T4bar1 =  requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_METALBAR_LEVEL1@1?locations="+city+"&qualities=0")
+    T4bar1Price = T4bar1.json()
+    T4bar2 =  requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_METALBAR_LEVEL2@2?locations="+city+"&qualities=0")
+    T4bar2Price = T4bar2.json()
+    T4bar3 =  requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T4_METALBAR_LEVEL3@3?locations="+city+"&qualities=0")
+    T4bar3Price = T4bar3.json()
+
+
+    result["T4bar"] = T4barPrice[0]['sell_price_min']
+    result["T4bar1"] = T4bar1Price[0]['sell_price_min']
+    result["T4bar2"] = T4bar2Price[0]['sell_price_min']
+    result["T4bar3"] = T4bar3Price[0]['sell_price_min']
+
+
+    return result
+
+
+def getT5Data(city): 
+    dataRaw = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T6_JOURNAL_MAGE_FULL?locations=Fortsterling&qualities=0")
+    data = dataRaw.json()
+    return data[0]['sell_price_min']
+
+
+def getT6Data(city): 
+    dataRaw = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T6_JOURNAL_MAGE_FULL?locations=Fortsterling&qualities=0")
+    data = dataRaw.json()
+    return data[0]['sell_price_min']
+
+def getT7Data(city): 
+    dataRaw = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T6_JOURNAL_MAGE_FULL?locations=Fortsterling&qualities=0")
+    data = dataRaw.json()
+    return data[0]['sell_price_min']
+
+def getT8Data(city): 
     dataRaw = requests.get("https://www.albion-online-data.com/api/v2/stats/Prices/T6_JOURNAL_MAGE_FULL?locations=Fortsterling&qualities=0")
     data = dataRaw.json()
     return data[0]['sell_price_min']
